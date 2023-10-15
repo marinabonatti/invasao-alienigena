58cd47aa91f74b4b231eb7dd5eb68e80efaa478c
Marina Bonatti <marina.bonatti@ascential.com>
2023-10-15 17:02:42 -0300

    Refatoração - loop principal do jogo + comentários

--- invasao_alienigena.py
+++ invasao_alienigena.py
@@ -21,8 +21,8 @@ class InvasaoAlienigena:
         self.configuracoes = Configuracoes()
 
         # Cria o display
-        self.tela = self.configuracoes.tela  # Surface
-        self.tela_rect = self.configuracoes.tela_rect # Rect
+        self.tela = self.configuracoes.tela 
+        self.tela_rect = self.configuracoes.tela_rect
 
         # Instancia classes necessárias
         self.stats = StatsJogo(self)
@@ -34,12 +34,17 @@ class InvasaoAlienigena:
         self.fim_jogo = FimJogo(self)
 
     def _criar_alien(self, n_alien, coluna):
+        """Cria um único alien, posiciona-o de acordo com os parâmetros
+        passados e adiciona-o ao grupo Sprite 'aliens'."""
         alien = Aliens(self)
         alien.rect.y = self.altura_alien + (2 * self.altura_alien * coluna)
         alien.rect.x = self.largura_alien + (2 * self.largura_alien * n_alien)
         self.aliens.add(alien)
 
     def criar_frota_alienigena(self):
+        """Cria uma frota alienígena de acordo com o espaço disponível
+        na tela. O tamanho da frota será proporcional à tela na qual o 
+        jogador está jogando - pois é FULLSCREEN."""
         alien = Aliens(self)
         self.largura_alien = alien.rect.width
         self.altura_alien = alien.rect.height
@@ -57,29 +62,37 @@ class InvasaoAlienigena:
         self.aliens.draw(self.tela)
 
     def validar_fim_jogo(self):
+        """Retorna True se a partida acabou - ou seja, se não há naves
+        restantes."""
         if self.stats.naves_restantes == 0:
             return True
     
     def reagir_fim_jogo(self):
-            if self.stats.pontuacao > self.stats.highscore:
-                self.stats.highscore = int(round(self.stats.pontuacao,-1))
-            self.stats.resetar_stats()
-            self.placar.preparar_placar()
-            self.placar.preparar_nivel()
-            self.placar.preparar_highscore()
-            self.configuracoes.jogo_ativo = False
-
+        """Reinicia os stats (naves restantes, pontuação e nível), salva
+        a pontuação atual como highscore se ela de fato for um highscore
+        e altera a flag jogo_ativo para False: o jogador deve escolher
+        se irá jogar novamente."""
+        if self.stats.pontuacao > self.stats.highscore:
+            self.stats.highscore = int(round(self.stats.pontuacao,-1))
+        self.stats.resetar_stats()
+        self.placar.preparar_placar()
+        self.placar.preparar_nivel()
+        self.placar.preparar_highscore()
+        self.configuracoes.jogo_ativo = False
 
     def _verificar_bordas_frota(self):
         """Reage adequadamente caso algum alien tenha atingido a borda
-        da tela."""
-
+        da tela, mudando a direção da frota e fazendo com que ela desça
+        em direção à nave do jogador."""
         for alien in self.aliens.sprites():
             if alien.verificar_bordas():
                 alien._mudar_direcao_frota()
                 break
     
     def verificar_colisoes(self):
+        """Verifica os três tipos de colisões possíveis no jogo: laser
+        colidindo com alien, alien colidindo com a nave e alien chegando
+        no chão. Para cada colisão haverá a reação esperada."""
         for alien in self.aliens.sprites():
             alien._verificar_colisao_aliens_lasers()
             alien._verificar_colisao_aliens_nave()
@@ -94,6 +107,28 @@ class InvasaoAlienigena:
         self.aliens.update()
         self.verificar_colisoes()
 
+    def disponibilizar_start(self):
+        """Disponibiliza o botão Start caso o jogo não esteja ativo."""
+        if not self.configuracoes.jogo_ativo:
+            self.botao_start = BotaoStart(self)
+            self.botao_start.desenhar()
+
+    def atualizar_tela(self):
+        """Roda as funcionalidades do jogo caso ele esteja ativo,
+        atualizando a tela conforme necessário."""
+        if self.configuracoes.jogo_ativo:
+            self.placar.mostrar_placar()
+            self.placar.preparar_naves_restantes()
+            self.nave.desenhar()
+            self.nave.atualizar_pos_nave(self)
+            self.criar_frota_alienigena()
+            self._atualizar_aliens()
+            for laser in self.lasers.sprites():
+                laser.desenhar()
+            self.lasers.update(self.lasers)
+            if self.validar_fim_jogo():
+                self.reagir_fim_jogo()
+
 
     def rodar_jogo(self):
         """Executa o loop principal do jogo."""
@@ -105,24 +140,9 @@ class InvasaoAlienigena:
 
             self.eventos.reagir_a_eventos(self)
 
-            # Disponibiliza o botão Start caso o jogo não esteja ativo
-            if not self.configuracoes.jogo_ativo:
-                self.botao_start = BotaoStart(self)
-                self.botao_start.desenhar()
+            self.disponibilizar_start()
 
-            # Roda as funcionalidades do jogo caso ele esteja ativo
-            elif self.configuracoes.jogo_ativo:
-                self.placar.mostrar_placar()
-                self.placar.preparar_naves_restantes()
-                self.nave.desenhar()
-                self.nave.atualizar_pos_nave(self)
-                self.criar_frota_alienigena()
-                self._atualizar_aliens()
-                for laser in self.lasers.sprites():
-                    laser.desenhar()
-                self.lasers.update(self.lasers)
-                if self.validar_fim_jogo():
-                    self.reagir_fim_jogo()
+            self.atualizar_tela()
 
             # Atualiza a tela com as últimas atualizações de display
             pygame.display.flip()
